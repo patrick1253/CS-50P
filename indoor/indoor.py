0bd4fb77e1ac28d633618307c0e429080f82b71c
patrick1253 <patricksdata@gmail.com>
2025-04-08 07:12:35 -0400

    indoor folder moved to Week_0 dir

--- indoor/indoor.py
+++
@@ -1,8 +0,0 @@
-#CS50P assignment: implement a program in Python that prompts the user for input and then 
-# outputs that same input in lowercase. Punctuation and whitespace should be outputted unchanged.
-
-def main():
-    shout = input("Say something in all caps: ").lower()
-    print(shout)
-
-main()
