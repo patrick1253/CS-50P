13a7c275a215ddb795e3a9754e6d093d2c04a136
patrick1253 <patricksdata@gmail.com>
2025-04-21 02:52:06 -0400

    simple test for bank program

---
+++ Week_5/test_bank/test_bank.py
@@ -0,0 +1,12 @@
+# CS50 assignment: In a file called bank.py, reimplement Home Federal Savings Bank 
+# from Problem Set 1, restructuring your code per the below, wherein value expects 
+# a str as input and returns an int, namely 0 if that str starts with “hello”, 20 if 
+# that str starts with an “h” (but not “hello”), or 100 otherwise, treating the str 
+# case-insensitively. You can assume that the string passed to the value function will n
+# ot contain any leading spaces. Only main should call print.
+
+# Then, in a file called test_bank.py, implement three or more functions that collectively 
+# test your implementation of value thoroughly, each of whose names should begin with test_ 
+# so that you can execute your tests with: "pytest test_bank.py"
+
+
