6244d389ed149ffe977e96564cebe4772de5b8f8
patrick1253 <patricksdata@gmail.com>
2025-04-24 11:23:50 -0400

    cannot get this to work yet

--- Week_6/lines/test_lines_alt.py
+++ Week_6/lines/test_lines_alt.py
@@ -1,10 +1,12 @@
 # CS50 assignment:  voluntarily adding a unit test for lines_alt.py, just to practice.
 
-import lines_alt
+from lines_alt import valid_file, count_lines
+#import lines_alt 
+import pytest
 
-def lines_alt_noPyExt():
-    with pytest.raises(ValueError):
-        lines_alt.valid_file("lines")
+def test_lines_alt_noPyExt():
+    with pytest.raises(NameError):
+        valid_file("['lines']")
 
-def lines_alt_correct():
-    assert lines_alt.valid_file("lines.py") == "27"
+def test_lines_alt_correct():
+    assert count_lines("lines.py") == "27"
