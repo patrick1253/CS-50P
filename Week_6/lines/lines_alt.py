09ecf181dbc139326187cfbce4a91391a3735742
patrick1253 <patricksdata@gmail.com>
2025-04-24 09:51:57 -0400

    still bugged in line 39

---
+++ Week_6/lines/lines_alt.py
@@ -0,0 +1,48 @@
+# CS50 assignment: in a file called lines.py, implement a program that expects exactly one 
+# command-line argument, the name (or path) of a Python file, and outputs the number of lines 
+# of code in that file, excluding comments and blank lines. If the user does not specify exactly 
+# one command-line argument, or if the specified file’s name does not end in .py, or if the 
+# specified file does not exist, the program should instead exit via sys.exit.
+
+# Assume that any line that starts with #, optionally preceded by whitespace, is a comment. (A docstring 
+# should not be considered a comment.) Assume that any line that only contains whitespace is blank.
+
+import sys
+
+def main():
+    filename = sys.argv[1:]
+    line_count = lines(filename)
+    print(f"Number of lines in {filename[0]}: {line_count}")
+
+def lines(filename):
+    
+    line_count = 0
+    
+    if (len(filename)) > 1:
+        sys.exit("Too many command-line arguments")
+    else:
+        filename = filename[0]    
+
+    suffix = (filename.split('.')[-1])
+          
+    if suffix != "py":
+        sys.exit("Not a python filename")
+
+    try:
+        file = open(filename, "r")
+    except FileNotFoundError:
+        sys.exit("File does not exist")  
+    
+    for line in file:
+        if line.lstrip() == "#":
+            pass
+        elif line in ["\n", "\t", "\r\n", "\n\r", "\t\n", "\t\r", "\t\n\r", "\t\r\n"]: # i.e. if line is blank
+            pass
+        else:
+            line_count += 1
+    
+    return line_count
+
+
+if __name__ == "__main__":
+    main()
