4db55c074b5ee44de642778aef26295be5c940c2
patrick1253 <patricksdata@gmail.com>
2025-04-25 07:05:10 -0400

    pycache commits

--- Week_6/lines/test_lines_alt.py
+++ Week_6/lines/test_lines_alt.py
@@ -1,12 +1,12 @@
 # CS50 assignment:  voluntarily adding a unit test for lines_alt.py, just to practice.
 
-from lines_alt import valid_file, count_lines
-#import lines_alt 
+#from lines_alt import valid_file, count_lines
+import lines_alt 
 import pytest
 
 def test_lines_alt_noPyExt():
     with pytest.raises(NameError):
-        valid_file("['lines']")
+        lines_alt("['lines_alt.py', 'lines']")
 
 def test_lines_alt_correct():
     assert count_lines("lines.py") == "27"
