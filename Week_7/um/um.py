7662e3124faa436f1e983edac69da2cc9d573ff4
patrick1253 <patricksdata@gmail.com>
2025-05-06 06:06:00 -0400

    initial commit app to count UMs in string

---
+++ Week_7/um/um.py
@@ -0,0 +1,28 @@
+# CS50 assignment: implement a function called count that expects a line of text as input 
+# as a str and returns, as an int, the number of times that “um” appears in that text, 
+# case-insensitively, as a word unto itself, not as a substring of some other word. For 
+# instance, given text like hello, um, world, the function should return 1. Given text 
+# like yummy, though, the function should return 0.
+
+# Structure um.py as follows, wherein you’re welcome to modify main and/or implement other 
+# functions as you see fit, but you may not import any other libraries. You’re welcome, but 
+# not required, to use re and/or sys.
+
+import re
+import sys
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    
+    match_list = []
+    if matches := re.findall(r"[\s, \,\s]*um[\,, \s]", s, re.IGNORECASE):
+    #if matches := re.findall(r"\bum\b", s, re.IGNORECASE):
+        for match in matches:
+            match_list.append(matches)
+    return len(match_list)
+
+if __name__ == "__main__":
+    main()
